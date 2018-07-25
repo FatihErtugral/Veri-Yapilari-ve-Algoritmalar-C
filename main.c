d729727a06c8b04f49598ab85d6562f080aabdef
FatihErtugral <fatih.ertugral@outlook.com>
2018-07-25 22:13:30 +0300

    ilk commit

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct LIST{
+	int data;
+	struct LIST *next;
+};
+typedef struct LIST node;
+
+int main(int argc, char *argv[]) {
+	node *root;
+	root = (node *)malloc(sizeof(node));
+	root -> data = 10;
+	root -> next = (node *)malloc(sizeof(node));
+	root -> next -> data = 20;
+	root -> next -> next = (node *)malloc(sizeof(node));
+	root -> next -> next -> data = 30;
+
+	node *iter;
+	iter = root;
+	printf("%d", iter -> data);
+	iter = iter -> next;
+	printf("%d", iter -> data);
+	iter = iter -> next;
+	printf("%d", iter -> data);
+	
+	getchar();
+}
