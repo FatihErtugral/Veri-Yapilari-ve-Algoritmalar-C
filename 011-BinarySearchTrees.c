ab6874d7730fe57f12b10ec28c44382dbda03cd4
FatihErtugral <fatih.ertugral@outlook.com>
2018-07-29 22:15:23 +0300

    sil fonksiyonu eklendi

--- 011-BinarySearchTrees.c
+++ 011-BinarySearchTrees.c
@@ -22,7 +22,7 @@ typedef struct BinarySearchTrees{
 	struct BinarySearchTrees *right;
 } NODE;
 
-NODE * ekle(NODE *agac, int veri){
+NODE *ekle(NODE *agac, int veri){
 	if(agac == NULL){
 		NODE *root = (NODE *)malloc(sizeof(NODE));
 		root->right = NULL;
@@ -68,6 +68,47 @@ int max(NODE *agac){
 		agac = agac->right;
 	return agac->data;
 }
+NODE *sil(NODE *agac, int veri){
+	/*	case 1: NULL	
+	 *	case 2: No child
+	 *	case 3: One child
+	 *	case 4: Two children
+	 */
+	if(agac == NULL)
+		return NULL; //----------------------------------	case 1: NULL
+	if(agac->data < veri){
+		agac->right = sil(agac->right, veri);
+		return agac;
+	}
+	if(agac->data > veri){
+		agac->left = sil(agac->left, veri);
+		return agac;
+	}
+	if(agac->data == veri){
+		if(agac->right == NULL && agac->left == NULL){ //-	case 2: No child
+			free(agac);
+			return NULL;
+		}
+		if(agac->left != NULL){ //------------------------	case 3 and case 4
+			int temp = max(agac->left);
+			agac->data = temp;
+			agac->left = sil(agac->left, temp);
+			return agac;
+		}
+		if(agac->right !=NULL){ //------------------------	case 3 and case 4
+			int temp = min(agac->right);
+			agac->data = temp;
+			agac->right = sil(agac->right, temp);
+			return agac;
+		}
+	}
+	/*	Silme işlemi performans bakımından daha iyi yapılabilir.
+	 *	Mevcut silme fonksiyonu, özyineli olduğu için,
+	 *	ortalarda çok çocuklu bir bağı silme işleminde, 
+	 *	sürekli diğer bağlar arasında, yer değiştirme işlemi yapmaktadır.
+	 *	Ayrıca her silme işleminde ağacın yapısı değişmektedir.
+	 */
+}
 int main(int argc, char *argv[]) {
 	NODE *agac = NULL;
 	agac = ekle(agac, 12);
@@ -80,6 +121,34 @@ int main(int argc, char *argv[]) {
 	agac = ekle(agac, 27);
 	agac = ekle(agac, 28);
 	dolas(agac);
-	printf("min: %d max: %d", min(agac), max(agac));
+	printf("min: %d max: %d\n", min(agac), max(agac));
+	agac = sil(agac, 200);
+	agac = sil(agac, 28);
+	agac = sil(agac, 56);
+	agac = sil(agac, 57);
+	dolas(agac);
+	printf("min: %d max: %d\n", min(agac), max(agac));
+	agac = ekle(agac, 80);
+	agac = ekle(agac, 90);
+	agac = ekle(agac, 100);
+	agac = ekle(agac, 110);
+	agac = ekle(agac, 120);
+	agac = ekle(agac, 105);
+	agac = ekle(agac, 95);
+	agac = ekle(agac, 85);
+	agac = ekle(agac, 75);
+	dolas(agac);
+	printf("min: %d max: %d\n", min(agac), max(agac));
+	agac = sil(agac, 12);
+	agac = sil(agac, 200);
+	agac = sil(agac, 190);
+	agac = sil(agac, 213);
+	agac = sil(agac, 56);
+	agac = sil(agac, 24);
+	agac = sil(agac, 18);
+	agac = sil(agac, 27);
+	agac = sil(agac, 28);
+	dolas(agac);
+	printf("min: %d max: %d\n", min(agac), max(agac));
 	return 0;
 }
